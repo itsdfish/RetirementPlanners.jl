9b5b5f9e21ca5f4f3da07db8f76a2ac39cb810da
Documenter.jl <documenter@juliadocs.github.io>
2024-02-01 12:08:30 +0000

    build based on 4ea9ef9

---
+++ dev/assets/logo_gen.jl
@@ -0,0 +1,45 @@
+cd(@__DIR__)
+using Pkg 
+Pkg.activate("../../..")
+using Random 
+using RetirementPlanners
+using Plots 
+Random.seed!(25)
+
+gdm = GBM(; μ = .07, σ = .12)
+trajectories = rand(gdm, 365 * 10, 4; Δt = 1 / 365)
+plot(
+    trajectories,
+    leg = false, 
+    grid = false, 
+    framestyle = :none,
+    # colors = [
+    #     RGB(.251, .388, .847)
+    #     RGB(.584, .345, .698)
+    #     RGB(.220, .596, .149)
+    #     RGB(.796, .235, .200)
+    # ]
+    size = (600,300),
+    background_color=:transparent,
+    dpi = 300,
+)
+
+savefig("logo.svg")
+
+plot(
+    trajectories,
+    leg = false, 
+    grid = false, 
+    framestyle = :none,
+    # colors = [
+    #     RGB(.251, .388, .847)
+    #     RGB(.584, .345, .698)
+    #     RGB(.220, .596, .149)
+    #     RGB(.796, .235, .200)
+    # ]
+    size = (1200,300),
+    background_color=:transparent,
+    dpi = 300,
+)
+
+savefig("large_logo.svg")
