a0b1ddddac4b1ab74331fec7efae20152ae5cc4f
itsdfish <itsdfish@gmail.com>
2024-02-02 08:03:49 -0500

    fix ext issue

--- ext/PlotsExt.jl
+++ ext/PlotsExt.jl
@@ -1,13 +1,14 @@
 module PlotsExt
-
-    using DataFrames
+ 
+    using RetirementPlanners
     using Distributions
     using Plots
-    using RetirementPlanners
     using SmoothingSplines
+    using DataFrames
 
     import RetirementPlanners: plot_gradient
     import RetirementPlanners: plot_sensitivity
+    import RetirementPlanners: to_dataframe
 
     """
         plot_gradient(
@@ -128,5 +129,43 @@ module PlotsExt
             fill = (true, cgrad(:RdYlGn_9, scale = :log10, rev=false));
             kwargs...
         )
+    end
+
+    """
+        to_dataframe(model::AbstractModel, data::Tuple)
+
+    Converts output from `grid_search` to a long-form dataframe. 
+
+    # Arguments
+
+    - `model::AbstractModel`: a subtype of abstract model for performing Monte Carlo simulations of retirement scenarios 
+    - `data::Tuple`: data output from `grid_search`
+    """
+    function to_dataframe(model::AbstractModel, data::Tuple)
+        times = get_times(model)
+        fields = fieldnames(typeof(data[2]))
+        n_fields = length(fields)
+        n_steps,n_reps = size(getfield(data[2], fields[1]))
+        df = DataFrame(
+            time = repeat(times, n_reps),
+            rep = repeat(1:n_reps, inner=n_steps)
+        )
+        for f ∈ fields 
+            df[!,f] = getfield(data[2], f)[:]
+        end
+        for k ∈ data[1]
+            col_name = make_unique_name(k[1])
+            df[!,col_name] .= k[2]
+        end
+        return df
+    end
+    
+    function to_dataframe(model, data)
+        return mapreduce(x -> to_dataframe(model, x), append!, data)
+    end
+    
+    function make_unique_name(np_keys)
+        str = split(String(np_keys[1]), "kw_")[2]
+        return Symbol(str * "_" * String(np_keys[2]))
     end
 end
